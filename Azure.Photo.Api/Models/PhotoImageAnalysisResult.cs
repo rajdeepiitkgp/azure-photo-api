b7342c5e50d975b327172d989b97c4ed9df239ea
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-12-07 22:07:04 +0100

    Added json property

--- Azure.Photo.Api/Models/PhotoImageAnalysisResult.cs
+++ Azure.Photo.Api/Models/PhotoImageAnalysisResult.cs
@@ -1,9 +1,11 @@
 using System.Security.Policy;
+using Newtonsoft.Json;
 
 namespace Azure.Photo.Api.Models;
 
 public class PhotoImageAnalysisResult
 {
+    [JsonProperty("id")]
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Url { get; set; } = string.Empty;
     public string Caption { get; set; } = string.Empty;
