1c172367d69088ab6eeb7baa787da2b558ac1a4a
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2024-12-08 23:19:58 +0100

    Added application insights logging

--- Azure.Photo.Api/Program.cs
+++ Azure.Photo.Api/Program.cs
@@ -1,38 +1,24 @@
 using Azure.Photo.Api.Controllers;
 using Azure.Photo.Api.Extensions;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using Scalar.AspNetCore;
-using Serilog;
-using LoggerExtensions = Azure.Photo.Api.Extensions.LoggerExtensions;
 
-LoggerExtensions.SetupSerilog();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.RegisterDependencies(builder.Configuration);
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Logging.AddApplicationInsights((config) => config.ConnectionString = builder.Configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING"), (options) => { });
+builder.Logging.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
 
-try
-{
-    Log.Information("Application Starting Up");
-    var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddOpenApi();
-    builder.Services.RegisterDependencies(builder.Configuration);
-    builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-    builder.Services.AddSerilog();
+var app = builder.Build();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
-    var app = builder.Build();
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+app.UseHttpsRedirection();
+app.UseCors();
+app.MapControllers();
 
-    app.UseHttpsRedirection();
-    app.UseCors();
-    app.UseSerilogRequestLogging();
-    app.MapControllers();
-
-    await app.RunAsync();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "The Application Failed To Start Correctly");
-}
-finally
-{
-    await Log.CloseAndFlushAsync();
-}
+await app.RunAsync();
 
 
